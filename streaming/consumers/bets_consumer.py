f5d96837b67466c15a7e113f11662d7bf0a2489d
Imade Osifo <imade.jeremiah@gmail.com>
2026-04-16 22:12:17 -0400

    set up stream events in postgres

--- streaming/consumers/bets_consumer.py
+++ streaming/consumers/bets_consumer.py
@@ -1,5 +1,6 @@
 import json
 from kafka import KafkaConsumer
+from ingestion.api_clients.postgres_client import get_postgres_connection
 
 
 consumer = KafkaConsumer(
@@ -12,7 +13,47 @@ consumer = KafkaConsumer(
 )
 
 
+def insert_bet_event(event):
+    conn = get_postgres_connection()
+    cur = conn.cursor()
+
+    insert_sql = """
+        INSERT INTO raw.bet_events_stream (
+            bet_id,
+            user_id,
+            game_id,
+            market_type,
+            selection,
+            stake,
+            odds,
+            bet_time
+        )
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+        ON CONFLICT (bet_id) DO NOTHING
+    """
+
+    cur.execute(
+        insert_sql,
+        (
+            event["bet_id"],
+            event["user_id"],
+            event["game_id"],
+            event["market_type"],
+            event["selection"],
+            event["stake"],
+            event["odds"],
+            event["bet_time"],
+        ),
+    )
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
 print("Listening for bet events...")
 
 for message in consumer:
-    print(f"Received: {message.value}")
+    event = message.value
+    insert_bet_event(event)
+    print(f"Inserted into Postgres: {event}")
